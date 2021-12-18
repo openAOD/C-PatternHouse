fabad8d1c762edd297345c9fcefd978e65dfd5a8
Bobby Kumar <kashyapashish0002@gmail.com>
2021-12-18 17:50:23 +0530

    Added code for pyramid pattern from 1 to 10.

---
+++ Pyramid Patterns/pyramidpattern7.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+void printPattern (int);
+void printWhitespaces (int);
+
+int main () {
+
+    int totalLines;
+
+    printf("How many lines to be printed? -- ");
+    fflush(stdout);
+    scanf("%d", &totalLines);
+
+    printf("\nYour pattern looks like this:\n");
+    printf("\n");
+
+    printPattern (totalLines);
+
+    printf("\n");
+    return 0;
+}
+
+void printPattern (int totalLines) {
+
+    int whiteSpace, printStar;
+
+    for (int i = totalLines; i >= 1; i--) {
+
+        whiteSpace = (totalLines - i);
+        printWhitespaces (whiteSpace);
+
+        printStar = ((2 * i) - 1);
+
+        for (int j = 1; j <= printStar; j++) {
+
+            if (j & 1) {
+
+                printf ("%2d ", i);
+            } else {
+
+                printf ("   ");
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+void printWhitespaces (int total) {
+
+    for (int i = 1; i <= total; i++) {
+
+        printf("   ");
+    }
+}
