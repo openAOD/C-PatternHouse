6b7ec6fdb053b441474dc3efad853b138aef02ff
Arkaprava Nath <natharkaprava@gmail.com>
2022-01-18 18:52:58 +0530

    10 patterns added

---
+++ Alphabetic Patterns/alphabeticpattern119.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 2; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            for (int k = 1; k <= j; k++)
+            {
+                printf("%c", k + 64);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
---
+++ Alphabetic Patterns/alphabeticpattern120.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            for (int k = 1; k <= j; k++)
+            {
+                printf("%c", i + 64);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
