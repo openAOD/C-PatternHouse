567dad5b3f1c6bdb34145e5dcd1d3c69bafc4c5a
ISHITA ROY <75474643+ISHITA-ROY016@users.noreply.github.com>
2022-01-24 23:17:18 +0530

    Added 4 patterns

---
+++ Numeric Patterns/numericpattern229.c
@@ -0,0 +1,18 @@
+//Print "X"
+#include <stdio.h>
+int main() {
+    int height = 5;
+    int i, j;
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < height; j++) {
+            if (j == i || j == height-1-i) {
+                printf("%d ",height-j);
+            }
+            else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
