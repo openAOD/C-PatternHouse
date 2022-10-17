70245ee919da02a66ea6327beb48801bb6dca070
Chlorine Pentoxide <69286505+ChlorinePentoxide@users.noreply.github.com>
2022-10-17 13:55:56 +0530

    Fixing alphabeticpattern132.c

--- Alphabetic Patterns/alphabeticpattern132.c
+++ Alphabetic Patterns/alphabeticpattern132.c
@@ -1,43 +1,46 @@
+#include <stdio.h>
 
-int height = 7;
-int size = 4;
-int ch1 = 0;
-int ch2 = size / 2;
-char chr2 = 'A' + (height / 2) + 1;
+int main() {
 
-for (int i = 0; i < height; i++)
-{
+    int height = 7;
+    int size = 4;
+    int ch1 = 0;
+    int ch2 = size / 2;
+    char chr2 = 'A' + (height / 2) + 1;
 
-    if (i <= height / 2)
+    for (int i = 0; i < height; i++)
     {
-        char chr = 'A' + i;
-        for (int j = 0; j < size; j++)
-        {
 
-            if (j >= ch1)
-                printf("%c ", chr);
-            else
-                printf("  ");
+        if (i <= height / 2)
+        {
+            char chr = 'A' + i;
+            for (int j = 0; j < size; j++)
+            {
+
+                if (j >= ch1)
+                    printf("%c ", chr);
+                else
+                    printf("  ");
+            }
+            chr++;
+            ch1++;
+            printf("\n");
         }
-        chr++;
-        ch1++;
-        printf("\n");
+
+        else
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (j >= ch2)
+                    printf("%c ", chr2);
+                else
+                    printf("  ");
+            }
+            chr2++;
+            ch2--;
+            printf("\n");
+        }
     }
 
-    else
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if (j >= ch2)
-                printf("%c ", chr2);
-            else
-                printf("  ");
-        }
-        chr2++;
-        ch2--;
-        printf("\n");
-    }
-}
-
-return 0;
+    return 0;
 }
